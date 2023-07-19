85dd377ea3feaea74cc81e87a9f28a230c0ef173
Nikola <nikola.dubak@hotmail.com>
2023-07-19 21:21:23 +0200

    Izmenjene rute ka servisima da budu relativne u produkciji

--- stock-market/src/environments/environment.prod.ts
+++ stock-market/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  usersUrl: "https://banka-1.k8s.elab.rs/api/users",
-  stocksUrl: "https://banka-1.k8s.elab.rs/api/stocks",
-  forexUrl: "https://banka-1.k8s.elab.rs/api/forexes",
-  ordersUrl: "https://banka-1.k8s.elab.rs/api/orders",
-  capitalUrl: "https://banka-1.k8s.elab.rs/api/user-listings",
-  optionsUrl: "https://banka-1.k8s.elab.rs/api/options",
-  companiesUrl: "https://banka-1.k8s.elab.rs/api/companies",
-  contractsUrl: "https://banka-1.k8s.elab.rs/api/contracts",
-  contactsUrl: "https://banka-1.k8s.elab.rs/api/contacts",
-  accountsUrl: "https://banka-1.k8s.elab.rs/api/accounts"
+  usersUrl: "/api/users",
+  stocksUrl: "/api/stocks",
+  forexUrl: "/api/forexes",
+  ordersUrl: "/api/orders",
+  capitalUrl: "/api/user-listings",
+  optionsUrl: "/api/options",
+  companiesUrl: "/api/companies",
+  contractsUrl: "/api/contracts",
+  contactsUrl: "/api/contacts",
+  accountsUrl: "/api/accounts"
 };
